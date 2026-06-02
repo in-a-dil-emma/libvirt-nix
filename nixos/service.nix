979d3ced4149bfe26fe7cbaaa0d483857833e377
Emma <contact@emma.pizza>
2026-06-02 12:15:47 +0200

    add strong service ordering before libvirt guests

--- nixos/service.nix
+++ nixos/service.nix
@@ -3,7 +3,9 @@
   config.systemd.services."libvirt-nix" = {
     description = "Configure libvirtd";
     after = [ "libvirtd.service" ];
+    before = [ "libvirt-guests.service" ];
     requires = [ "libvirtd.service" ];
+    requiredBy = [ "libvirt-guests.service" ];
     wantedBy = [ "multi-user.target" ];
     script = config.libvirt-nix.mainScript.outPath;
   };
